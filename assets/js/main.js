3ed662eb99d6abf05de6eadc1370459eaeba61e8
Yessenia <yesel1601@hotmail.com>
2017-03-06 14:27:06 -0500

    First commmit dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
+var nombre = prompt("¿Cuál es tu nombre?");
+document.getElementById('name').innerHTML = nombre;
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}

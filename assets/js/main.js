9da324f4a53c27be18039fc2ad4257f43fe8ae4b
Yessenia <yesel1601@hotmail.com>
2017-03-06 16:19:16 -0500

    Second commit with buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,13 @@ window.onclick = function(event) {
 		}
 	}
 }
+
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
